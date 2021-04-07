03cd6a7d5661ef5ff66fe6511ae8d5bab09ae1da
DevCode-C <emmanuelparedescamargo@gmail.com>
2021-04-07 16:28:42 -0500

    Obtencion de los espacio de memoria de la entrada

--- MazeTraversal/main.c
+++ MazeTraversal/main.c
@@ -2,15 +2,20 @@
 #include <stdio.h>
 #include "mazeMap.h"
 
-extern unsigned char mazeZone[ROW][COLUMN];
+extern uint8_t mazeZone[ROW][COLUMN]; //Declaramos como extern a mazeZone
+// uint8_t Position_X;
+// uint8_t Position_Y;
+                                    //Declaramos dos punteros para obtener las posiciones de memoria delntro del mazeZone
+uint8_t *ptrPosition_X;             //Posicion de memoria en coordenadas x
+uint8_t *ptrPosition_y;             //Posicion de memoria en coordenadas y
 
 int main(void){
-    for(int i=0;i<12;i++){
-        for (int j = 0; j < 12; j++){
-            printf("%4c",mazeZone[i][j]);
-        }
-        printf("\n");   
-    }
-    printf("%4c\n",PLAYER);
+    showMazeZone(mazeZone);         //Funcion usada para mostrar por pantalla el mapa
+    printf("Direccion del punto X: %p\n Direccion del punto y: %p\n",&ptrPosition_X,&ptrPosition_y);
+   
+    searchDoor(mazeZone,&ptrPosition_X,&ptrPosition_y);     //Funcion usada que busca la entrada
+
+    printf("Direccion del punto X: %c\n Direccion del punto y: %c\n",*ptrPosition_X,*ptrPosition_y);
+    printf("Direccion del punto X: %p\n Direccion del punto y: %p\n",ptrPosition_X,ptrPosition_y);
     return 0;
 }
--- MazeTraversal/mazeMap.h
+++ MazeTraversal/mazeMap.h
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 
 #ifndef __MAZEMAP_H
 #define __MAZEMAP_H 1
@@ -11,7 +12,8 @@
 #define PLAYER  '@'
 
 
+void showMazeZone(uint8_t (*mazeZoneArray)[COLUMN]);
 
-void searchDoor();
+void searchDoor(uint8_t (*mazeZoneArray)[COLUMN], uint8_t **GetPosition_X, uint8_t **GetPosition_Y);
 
 #endif
