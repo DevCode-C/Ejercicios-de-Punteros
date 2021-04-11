e304ba528bfe6572914231bd511130f0de22c66c
DevCode-C <emmanuelparedescamargo@gmail.com>
2021-04-11 03:18:18 -0500

    New functions

--- CardShufflingV2/deck.h
+++ CardShufflingV2/deck.h
@@ -4,26 +4,27 @@
 
 #ifndef __DECK__H
 #define __DECK__H
-#define SUIT 4 
-#define FACES 13
-#define CARDS 52
 
-void shuffle(unsigned int wDeck[][FACES]);
-void deal(unsigned int wDeck[][FACES], const char *wFaces[], const char *wSuit[]);
+    #define     SUIT    4 
+    #define     FACES   13
+    #define     CARDS   52
 
-void dealHand(void);
+    void shuffle(unsigned int wDeck[][FACES]);
+    void deal(unsigned int wDeck[][FACES], const char *wFaces[], const char *wSuit[]);
 
-void printDeck(unsigned int wDeck[][FACES]);
+    void dealHand(void);
 
-void handPairs(void);
+    void printDeck(unsigned int wDeck[][FACES]);
 
-void handTwoPairs(void);
+    void handPairs(void);
 
-void handThreeJack(void);
+    void handTwoPairs(void);
 
-void handFourAces(void);
+    void handThreeJack(void);
 
-void handFlush(void);
+    void handFourAces(void);
 
-void handStraight(void);
+    void handFlush(void);
+
+    void handStraight(void);
 #endif
