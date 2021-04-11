f5ed52726774d4f50342d8d9082fd9d7f358af75
DevCode-C <emmanuelparedescamargo@gmail.com>
2021-04-11 03:17:01 -0500

    New functions

--- CardShufflingV2/deck.c
+++ CardShufflingV2/deck.c
@@ -1,1 +1,40 @@
 #include "deck.h"
+
+void shuffle(unsigned int wDeck[][FACES]){
+    for(unsigned char card = 1; card <= CARDS; ++card){
+        unsigned char row;
+        unsigned char column;
+
+        do{
+            row = rand() % SUIT;
+            column = rand() % FACES;
+            // printf("ROW: %d \tCOLUMN: %d\n",row,column);
+        } while (wDeck[row][column] != 0);
+        
+        wDeck[row][column] = card;
+    }
+}
+
+
+void deal(unsigned int wDeck[][FACES], const char *wFaces[], const char *wSuit[]){
+    for(unsigned char card = 1; card <=CARDS; ++card){
+        for(unsigned char row = 0; row < SUIT; ++row){
+            for(unsigned char column = 0; column < FACES; ++column){
+                if (wDeck[row][column] == card){
+                    printf("%5s of %-8s%c", wFaces[column], wSuit[row],card % 2 == 0 ? '\n' : '\t');
+                }
+            }
+        }
+    }
+}
+
+void printDeck(unsigned int wDeck[][FACES]){
+    for (unsigned char i = 0; i < SUIT; i++){
+        for (unsigned char j = 0; j < FACES; j++){
+            printf("%s\t",wDeck[SUIT][FACES]);
+        }
+        printf("\n");
+        
+    }
+    
+}
--- CardShufflingV2/deck.h
+++ CardShufflingV2/deck.h
@@ -1,7 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #ifndef __DECK__H
 #define __DECK__H
+#define SUIT 4 
+#define FACES 13
+#define CARDS 52
 
+void shuffle(unsigned int wDeck[][FACES]);
+void deal(unsigned int wDeck[][FACES], const char *wFaces[], const char *wSuit[]);
+
+void dealHand(void);
+
+void printDeck(unsigned int wDeck[][FACES]);
+
+void handPairs(void);
+
+void handTwoPairs(void);
+
+void handThreeJack(void);
+
+void handFourAces(void);
+
+void handFlush(void);
+
+void handStraight(void);
 #endif
